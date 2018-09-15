55845acd2df031365a4a285a4a913b6cc126fdb6
12jeong <haniijeong827@gmail.com>
2018-09-15 15:20:08 +0900

    0915

--- nonstationary_reg_musigma.R
+++ nonstationary_reg_musigma.R
@@ -27,7 +27,67 @@ maxiter = 1000
 tol = 1e-4
 expr = expr_reg
 
-#######
+
+###################################################
+#### Beta newton method using GEVnewtonRaphson ####
+
+GEVnewtonRaphson_reg2 <- function (x, z, theta0, expr, alpha=1, maxiter=1000, tol = 1e-4)
+{
+  old_theta <- theta0 
+  old_beta <- rep(0,ncol(z))
+  niter <- 0
+  alp <- seq(from=0,to=100,by=1)/100
+  
+  Jaco <- expr$Jaco
+  Hmat <- expr$Hmat
+  for (i in 1:maxiter) {
+    cat("iter::", i, '\n')
+    niter = niter + 1
+    # theta update
+    x_d = x- z %*% old_beta 
+    mu=old_theta[1]; sigma=old_theta[2]; k=old_theta[3]
+    fit_mle <- GEVnewtonRaphson(x = x_d, theta0 = c(mu,sigma,k), 
+                                alpha=1, expr = expr_mle, maxiter = 10, tol = 1e-05)
+    new_theta = fit_mle$root
+    cat("theta:", new_theta, '\n')
+    del_theta = new_theta - old_theta
+    v = lossfun(x = x - c(z%*%old_beta), new_theta[1], new_theta[2] ,new_theta[3])
+    cat("theta_update:::", v, '\n')
+    
+    # beta update
+    mu=c(new_theta[1]+z%*%old_beta); sigma=new_theta[2]; k=new_theta[3]
+    jvec_beta = apply(eval(Jaco)[,1]*z,2,sum)
+    #jvec_beta =colSums(z*c(grad_mu_mat(x , mu+z%*%old_beta , sigma, k)))
+    hmat_beta = eval(Hmat)[1,1] *t(z)%*%z
+    new_beta <- old_beta - alpha*solve(hmat_beta)%*%jvec_beta
+    
+    cat("grad_beta::", jvec_beta,"\n")
+    cat("beta:", new_beta, '\n')
+    v = lossfun(x = x - c(z%*%new_beta), new_theta[1], new_theta[2] ,new_theta[3])
+    cat("beta_update:::", v, '\n')
+    del_beta <- new_beta - old_beta
+    
+    # break rule
+    delta=c(del_theta,del_beta)
+    if ( norm(as.matrix(delta)) < tol ) {    ### if (max(abs(del_theta)) < tol )
+      break
+    }    
+    old_theta = new_theta
+    old_beta = new_beta
+    if (niter == maxiter) {
+      warning("Maximum number of iterations 'maxiter' was reached.")
+    }
+    #v = lossfun(x = x - c(z%*%new_beta), new_theta[1], new_theta[2] ,new_theta[3])
+    #cat("V:::", v, '\n')
+  }
+  return(list(initial = theta0, root = c(new_theta,new_beta), step = niter, del = delta))
+}
+
+GEVnewtonRaphson_reg2(x, z, theta0, expr, alpha=1, maxiter=1000)
+
+#####################################################
+#### Beta newton method without GEVnewtonRaphson ####
+
 GEVnewtonRaphson_reg3 <- function(xdata, z, theta0, expr, alpha=1, maxiter = 1000, tol = 1e-05) {
   old_theta <- theta0 
   old_beta <- rep(0,ncol(z))
