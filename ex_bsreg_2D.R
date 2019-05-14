409fb714c8d9778b0716b9165c5b82a0d1061a68
12jeong <haniijoeng827@gmail.com>
2019-05-14 14:44:34 +0900

    plot for lambda = 0, 0.01, 0.1, 0.5, 1, 2

--- ex_bsreg_2D.R
+++ ex_bsreg_2D.R
@@ -12,8 +12,8 @@ ns <- length(unique(Pr_46$stnlds))
 
 # create.bspline.basis : nbasis = norder + length(breaks) -2
 # order=4, quantile knots, nbasis=7, 
-x_bsobj <- create.bspline.basis(range(Pr_46$long),breaks=quantile(Pr_46$long,prob = seq(0, 1, length = 5)))
-y_bsobj <- create.bspline.basis(range(Pr_46$lat),breaks=quantile(Pr_46$lat,prob = seq(0, 1, length = 5)))
+x_bsobj <- create.bspline.basis(range(Pr_46$long),breaks=quantile(Pr_46$long,prob = seq(0, 1, length = 3)))
+y_bsobj <- create.bspline.basis(range(Pr_46$lat),breaks=quantile(Pr_46$lat,prob = seq(0, 1, length = 3)))
 
 zlist <- list()
 for (i in 1:ns){
@@ -24,19 +24,6 @@ for (i in 1:ns){
 }
 
 dim(zlist[[1]]) # (frist)stnlds 2D-splines tensor, nbasis = 7 x 7
-
-# n = nrow(zlist[[1]])
-# p = ncol(zlist[[1]])
-# true_theta=c(100,30,0.1)
-# true_beta = rep(1,p) # setting?
-# xlist = list()
-# for (i in 1:ns){
-#   z = zlist[[i]]
-#   eps = rgev(n,loc=true_theta[1], 
-#              scale=true_theta[2], 
-#              shape=true_theta[3])
-#   xlist[[i]] = z%*%true_beta + eps 
-# }
 
 # 2-D splines penlaty matrix
 Fmat <- kronecker(bsplinepen(x_bsobj,Lfdobj=2),bsplinepen(y_bsobj,Lfdobj=0))
@@ -59,14 +46,15 @@ for (i in (1: (ns-1))){
 }
 mat = t(matt) %*% matt
 
-# result1 <- round(gevreg_m(xlist, zlist, lambda = 0, Om=Om, method="B-spline"),3)
-# result2 <- round(gevreg_m(xlist, zlist, lambda = 0.01, Om=Om, method="B-spline"),3)
-result3 <- round(gevreg_m(xlist, zlist, lambda = 1, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
-result4 <- round(gevreg_m(xlist, zlist, lambda = 0.1, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
+Sys.time()
+result1 <- round(gevreg_m(xlist, zlist, lambda = 0, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
+Sys.time()
+result2 <- round(gevreg_m(xlist, zlist, lambda = 0.01, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
+result3 <- round(gevreg_m(xlist, zlist, lambda = 0.1, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
+result4 <- round(gevreg_m(xlist, zlist, lambda = 0.5, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
+result5 <- round(gevreg_m(xlist, zlist, lambda = 1, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
+result6 <- round(gevreg_m(xlist, zlist, lambda = 2, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
 
-# result1[1:(ns*3)]
-# result2[1:(ns*3)]
-# result3[1:(ns*3)]
 
 p <- ncol(zlist[[1]])
 x <- unique(Pr_46$long)
@@ -78,24 +66,19 @@ z1 <- tensorbss %*% tail(result1,p)
 z2 <- tensorbss %*% tail(result2,p)
 z3 <- tensorbss %*% tail(result3,p)
 z4 <- tensorbss %*% tail(result4,p)
-scatterplot3d(x,y,z4,scale.y=0.6,angle=10)
+z5 <- tensorbss %*% tail(result4,p)
+z6 <- tensorbss %*% tail(result4,p)
 
-plot3d(x,y,z4)
 # bsmat <- data.frame(x,y,z1,z2,z3)
-
-# x,y,z plot 그리기 
-# library(plotly)
-# library(rgl)
-# plot3d(x,y,z1,col="#999999")
-# points3d(x,y,z2,col="#E69F00")
-# points3d(x,y,z3,col="#56B4E9")
-
-library(scatterplot3d)
-par(mfrow=c(1,3))
-scatterplot3d(x,y,z1,scale.y=0.6,angle=10)
-scatterplot3d(x,y,z2,scale.y=0.6,angle=10)
-scatterplot3d(x,y,z3,scale.y=0.6,angle=10)
-
-
-save(result1,result2,result3,result4,file="ex_bsreg_2D.RData")
+# save(result1,result2,result3,result4,result5,result6,file="ex_bsreg_2D.RData")
 # load("ex_bsreg_2D.RData")
+library(scatterplot3d)
+par(mfrow=c(2,3))
+scatterplot3d(x,y,z1,scale.y=0.6,angle=10,main=0)
+scatterplot3d(x,y,z2,scale.y=0.6,angle=10,main=0.01)
+scatterplot3d(x,y,z3,scale.y=0.6,angle=10,main=0.1)
+scatterplot3d(x,y,z4,scale.y=0.6,angle=10,main=0.5)
+scatterplot3d(x,y,z5,scale.y=0.6,angle=10,main=1)
+scatterplot3d(x,y,z6,scale.y=0.6,angle=10,main=2)
+
+
