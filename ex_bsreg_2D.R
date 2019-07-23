735dc63f1b286923922f3426619a2b67d618dc3f
12jeong <haniijoeng827@gmail.com>
2019-07-23 14:49:08 +0900

    0723

--- ex_bsreg_2D.R
+++ ex_bsreg_2D.R
@@ -6,12 +6,15 @@ setwd("C:\\Users\\UOS\\Documents\\GITHUB\\gev")
 source("sgevlibrary.R")
 load("kma_data\\Pr_46.RData")
 
-ss <- split.data.frame(Pr_46,Pr_46$stnlds)
-xlist <- lapply(ss,"[[","pr")
+ss <- split.data.frame(Pr_46,Pr_46$stnlds)  # stnlds로 dataframe 쪼개서 list에 분배
+xlist <- lapply(ss,"[[","pr")               # 강수량(pr) 변수로만 이루어진 list 생성
 ns <- length(unique(Pr_46$stnlds))
 
 # create.bspline.basis : nbasis = norder + length(breaks) -2
-# order=4, quantile knots, nbasis=7, 
+# ex1 : order=4, quantile knots, nbasis=7 
+# ex2 : order=4, (0,0.5,1) knots, nbasis=5
+
+# x : long (경도) , y : lat (위도) 
 x_bsobj <- create.bspline.basis(range(Pr_46$long),breaks=quantile(Pr_46$long,prob = seq(0, 1, length = 3)))
 y_bsobj <- create.bspline.basis(range(Pr_46$lat),breaks=quantile(Pr_46$lat,prob = seq(0, 1, length = 3)))
 
@@ -19,11 +22,10 @@ zlist <- list()
 for (i in 1:ns){
   xbs <- eval.basis(ss[[i]]$long,x_bsobj)
   ybs <- eval.basis(ss[[i]]$lat,y_bsobj)
-  tensorbs <- do.call('cbind', lapply(1:ncol(xbs), function(i) xbs[, i] * ybs)) 
+  tensorbs <- do.call('cbind', lapply(1:ncol(xbs), function(i) xbs[, i] * ybs))  # row-wise kronecker product
   zlist[[i]] <- tensorbs 
 }
-
-dim(zlist[[1]]) # (frist)stnlds 2D-splines tensor, nbasis = 7 x 7
+dim(zlist[[1]]) # (frist)stnlds 2D-splines tensor, nbasis = 5 x 5
 
 # 2-D splines penlaty matrix
 Fmat <- kronecker(bsplinepen(x_bsobj,Lfdobj=2),bsplinepen(y_bsobj,Lfdobj=0))
@@ -46,13 +48,17 @@ for (i in (1: (ns-1))){
 }
 mat = t(matt) %*% matt
 
+
+
+
+
+## example 
 result1 <- round(gevreg_m(xlist, zlist, lambda = 0, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
-result2 <- round(gevreg_m(xlist, zlist, lambda = 0.1, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
-result3 <- round(gevreg_m(xlist, zlist, lambda = 0.3, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
-result4 <- round(gevreg_m(xlist, zlist, lambda = 0.5, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
+result2 <- round(gevreg_m(xlist, zlist, lambda = 0.2, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
+result3 <- round(gevreg_m(xlist, zlist, lambda = 0.4, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
+result4 <- round(gevreg_m(xlist, zlist, lambda = 0.6, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
 result5 <- round(gevreg_m(xlist, zlist, lambda = 0.8, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
 result6 <- round(gevreg_m(xlist, zlist, lambda = 1, lambda2=1, Om=Om, mat=mat, method="B-spline"),3)
-
 
 p <- ncol(zlist[[1]])
 x <- unique(Pr_46$long)
@@ -68,17 +74,17 @@ z5 <- tensorbss %*% tail(result5,p)
 z6 <- tensorbss %*% tail(result6,p)
 
 # bsmat <- data.frame(x,y,z1,z2,z3)
-# save(result1,result2,result3,result4,result5,result6,file="ex_bsreg_2D.RData")
+save(result1,result2,result3,result4,result5,result6,file="ex_bsreg_2D.RData")
 # load("ex_bsreg_2D.RData")
 library(scatterplot3d)
 par(mfrow=c(2,3))
 scatterplot3d(x,y,z1,scale.y=0.6,angle=10,main=0)
-scatterplot3d(x,y,z2,scale.y=0.6,angle=10,main=0.1)
-scatterplot3d(x,y,z3,scale.y=0.6,angle=10,main=0.3)
-scatterplot3d(x,y,z4,scale.y=0.6,angle=10,main=0.5)
+scatterplot3d(x,y,z2,scale.y=0.6,angle=10,main=0.2)
+scatterplot3d(x,y,z3,scale.y=0.6,angle=10,main=0.4)
+scatterplot3d(x,y,z4,scale.y=0.6,angle=10,main=0.6)
 scatterplot3d(x,y,z5,scale.y=0.6,angle=10,main=0.8)
 scatterplot3d(x,y,z6,scale.y=0.6,angle=10,main=1)
 
-
 library(rgl)
-plot3d(x,y,z1)
+plot3d(x,y,z3)
+plot3d(x,y,z4)
--- numerical_study_0705.R
+++ numerical_study_0705.R
@@ -1,14 +1,17 @@
+rm(list=ls())
+
 ########### 이변량정규분포3차원그래프 ##############
 if(!require(mvtnorm)) install.packages('mvtnorm'); require(mvtnorm)
 if(!require(plotly)) install.packages('plotly'); require(plotly)
 if(!require(evd)) install.packages('evd'); require(evd)
 
+# data generation
 set.seed(2019)
 n=100
 x1 = seq(-5,5,length.out=n)
 x2 = seq(-5,5,length.out=n)
-mu1 = c(0,-3)
-mu2 = c(0,3)
+mu1 = c(0,-2)
+mu2 = c(0,2)
 sig = matrix(c(2,1,1,2),nrow=2)
 fx = outer(x1, x2, function(x1,x2) { 
   0.4*dmvnorm(cbind(x1,x2),mean=mu1, sigma=sig) +
@@ -17,18 +20,98 @@ fx = outer(x1, x2, function(x1,x2) {
 plot_ly(x = x1, y = x2, z = fx) %>% add_surface()
 
 # matrix(1:n^2,nrow=n)
-ns = 100
+ns = 50
+set.seed(21)
 s_ind = sample(n^2,ns)
 s_row = trunc(s_ind/100)+1 # 1, 100, 101, 10000
 s_row[s_ind%%100==0] = s_ind[s_ind%%100==0]/100
 s_col = (s_ind/100-s_row+1)*100 
 zval = c()
 for ( i in 1:ns) { zval[i] =fx[s_row[i],s_col[i]] ;cat(i,fx[s_row[i],s_col[i]],"\n") }
-df = data.frame(row=x1[s_row],col=x2[s_col],z=100+zval*100)
-plot3d(x=df$row,y=df$col,z=df$z)
+df_mu = data.frame(x1=x1[s_col],x2=x2[s_row],z=100+zval*100)
+plot3d(x=df_mu$x1,y=df_mu$x2,z=df_mu$z)
 
 xlist = list()
 for (i in 1:ns){
-xlist[[i]] = rgev(n,loc=df$z[i],scale=40,shape=0.1)
+xlist[[i]] = rgev(n,loc=df_mu$z[i],scale=40,shape=0.1)
 }
 
+
+# train
+setwd("C:\\Users\\UOS\\Documents\\GITHUB\\gev")
+source("sgevlibrary.R")
+if(!require(fda)) install.packages('fda'); library(fda)
+
+# zlist - 2-D basis matrix by each location
+x_bsobj = create.bspline.basis(range(x1),breaks=quantile(x1,prob = seq(0, 1, length = 5)))
+y_bsobj = create.bspline.basis(range(x2),breaks=quantile(x2,prob = seq(0, 1, length = 5)))
+zlist = list()
+for (i in 1:ns){
+  xbs = eval.basis(df_mu$x1[i],x_bsobj)
+  ybs = eval.basis(df_mu$x2[i],y_bsobj)
+  tensorbs = do.call('cbind', lapply(1:ncol(xbs), function(i) xbs[, i] * ybs)) 
+  zlist[[i]] = tensorbs 
+}
+# dim(zlist[[1]]) # (frist)stnlds 2D-splines tensor, nbasis = df x df
+
+# Omega matrix
+Fmat = kronecker(bsplinepen(x_bsobj,Lfdobj=2),bsplinepen(y_bsobj,Lfdobj=0))
+Gmat = kronecker(bsplinepen(x_bsobj,Lfdobj=0),bsplinepen(y_bsobj,Lfdobj=2))
+Hmat = kronecker(bsplinepen(x_bsobj,Lfdobj=1),bsplinepen(y_bsobj,Lfdobj=1))
+Om = Fmat+Gmat+2*Hmat
+
+# optim control
+optim_controlList = list()
+optim_controlList$maxit = 1e+3
+
+# design matrix for v3 (mu_0 stationary)
+matfunc = function(ns){
+  matt = matrix(0,nrow=ns*(ns-1),ncol=ns)
+  k = 1
+  for (i in (1: (ns-1))){
+    for (j in ((i+1) :ns)){
+      matt[k,i] = 1
+      matt[k,j] = -1
+      k = k+1
+    }  
+  }
+  mat = t(matt) %*% matt
+  return(mat)
+}
+mat = matfunc(ns)
+
+# result1 = gevreg_m(xlist,zlist,
+#                    lambda = 1, lambda2=1, Om=Om, mat=mat, method="B-spline")
+# result2 = gevreg_m(xlist,zlist,
+#                    lambda = 0, lambda2=1, Om=Om, mat=mat, method="B-spline")
+# result3 = gevreg_m(xlist,zlist,
+#                    lambda = 0.5, lambda2=1, Om=Om, mat=mat, method="B-spline")
+
+# save(result1,result2,result3,file="result0705.RData")
+load("result0705.RData")
+
+p <- ncol(zlist[[1]])
+xbss <- eval.basis(x1[s_row],x_bsobj)
+ybss <- eval.basis(x2[s_col],y_bsobj)
+tensorbss <- do.call('cbind', lapply(1:ncol(xbss), function(i) xbss[, i] * ybss)) 
+z1 <- 100+tensorbss %*% tail(result1,p)
+z2 <- 100+tensorbss %*% tail(result2,p)
+z3 <- 100+tensorbss %*% tail(result3,p)
+
+library(scatterplot3d)
+par(mfrow=c(2,2))
+scatterplot3d(df_mu$x1,df_mu$x2,df_mu$z,scale.y=1,angle=40,main="True")
+scatterplot3d(df_mu$x1,df_mu$x2,z2,scale.y=1,angle=40,main="Est,0")
+scatterplot3d(df_mu$x1,df_mu$x2,z3,scale.y=1,angle=40,main="Est,0.5")
+scatterplot3d(df_mu$x1,df_mu$x2,z1,scale.y=1,angle=40,main="Est,1")
+scatterplot3d(df_mu$x2,df_mu$x1,df_mu$z,scale.y=1,angle=40,main="True")
+scatterplot3d(df_mu$x2,df_mu$x1,z2,scale.y=1,angle=40,main="Est,0")
+scatterplot3d(df_mu$x2,df_mu$x1,z3,scale.y=1,angle=40,main="Est,0.5")
+scatterplot3d(df_mu$x2,df_mu$x1,z1,scale.y=1,angle=40,main="Est,1")
+
+
+library(rgl)
+plot3d(df_mu$x1,df_mu$x2,df_mu$z)
+plot3d(df_mu$x1,df_mu$x2,z2,main="Est,0")
+plot3d(df_mu$x1,df_mu$x2,z3,main="Est,0.5")
+plot3d(df_mu$x1,df_mu$x2,z1,main="Est,1")
