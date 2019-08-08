eeb93b4bded732db8939acbf04fe337e31e1143e
12jeong <haniijoeng827@gmail.com>
2019-08-08 19:35:10 +0900

    plot 수정
    update plot z axis

--- sgev_train.R
+++ sgev_train.R
@@ -9,21 +9,21 @@ nobs = 100
 ns= 50
 ######################## mu setting ##########################
 ### setting1 : 평면 ###
-# xyrange = c(-10,10)
-# nBS = 3
-# x1 = seq(xyrange[1],xyrange[2],length=100)
-# x2 = seq(xyrange[1],xyrange[2],length=100)
-# fx = outer(x1, x2, function(x1,x2) {
-#   par_mu = 100+(-2*x1-3*x2)
-# })
-## plot_ly(x = x1, y = x2, z = fx) %>% add_surface()
-# range(fx)
-# x1 = runif(ns,xyrange[1],xyrange[2])
-# x2 = runif(ns,xyrange[1],xyrange[2])
-# par_mu = 100+(-2*x1-3*x2) # 평면
-# range(par_mu)
-# df_mu = data.frame(x1=x1,x2=x2,par_mu=par_mu)
-## plot3d(x=df_mu$x1,y=df_mu$x2,z=par_mu)
+xyrange = c(-10,10)
+nBS = 3
+x1 = seq(xyrange[1],xyrange[2],length=100)
+x2 = seq(xyrange[1],xyrange[2],length=100)
+fx = outer(x1, x2, function(x1,x2) {
+  par_mu = 100+(-2*x1-3*x2)
+})
+plot_ly(x = x1, y = x2, z = fx) %>% add_surface()
+range(fx)
+x1 = runif(ns,xyrange[1],xyrange[2])
+x2 = runif(ns,xyrange[1],xyrange[2])
+par_mu = 100+(-2*x1-3*x2) # 평면
+range(par_mu)
+df_mu = data.frame(x1=x1,x2=x2,par_mu=par_mu)
+plot3d(x=df_mu$x1,y=df_mu$x2,z=par_mu)
 
 ### setting2 : 일봉분포 ###
 # xyrange = c(-10,10)
@@ -36,7 +36,7 @@ ns= 50
 # fx = outer(x1, x2, function(x1,x2) {
 #   dmvnorm(cbind(x1,x2), mean=mu, sigma=sig)
 # })
-## plot_ly(x = x1, y = x2, z = fx) %>% add_surface()
+# plot_ly(x = x1, y = x2, z = 90+fx*4000) %>% add_surface()
 # 
 # set.seed(3)
 # s_ind = sort(sample(n^2,ns))
@@ -56,38 +56,38 @@ ns= 50
 # plot3d(x=df_mu$x1,y=df_mu$x2,z=par_mu)
 
 ### setting3 : 이봉분포 ###
-set.seed(1)
-nBS = 5
-xyrange = c(-10,10)
-n=100
-x1 = seq(xyrange[1],xyrange[2],length=n)
-x2 = seq(xyrange[1],xyrange[2],length=n)
-mu1 = c(5,0)
-mu2 = c(-5,0)
-sig = matrix(c(10,5,5,10),nrow=2)
-fx = outer(x1, x2, function(x1,x2) {
-  0.4*dmvnorm(cbind(x1,x2),mean=mu1, sigma=sig*1) +
-    0.6*dmvnorm(cbind(x1,x2),mean=mu2, sigma=sig*1)
-})
-# plot_ly(x = x1, y = x2, z = fx) %>% add_surface()
-
-set.seed(3)
-s_ind = sort(sample(n^2,ns))
-zval = rep(0, ns)
-k = 1
-for ( i in s_ind)
-{
-  zval[k] =fx[i]
-  k = k + 1
-}
-s_col = s_ind%/%n + 1 ; s_col[s_ind%/%n == n] = n
-s_row = s_ind%%n + 1 ; s_row[s_row == 0] = n
-par_mu = 85+zval*5000 # setting3_1 : 85~113, nBS=5
-# par_mu = 90+zval*4000 # setting3_2 : 90~112, nBS=5
+# set.seed(1)
+# nBS = 5
+# xyrange = c(-10,10)
+# n=100
+# x1 = seq(xyrange[1],xyrange[2],length=n)
+# x2 = seq(xyrange[1],xyrange[2],length=n)
+# mu1 = c(5,0)
+# mu2 = c(-5,0)
+# sig = matrix(c(10,5,5,10),nrow=2)
+# fx = outer(x1, x2, function(x1,x2) {
+#   0.4*dmvnorm(cbind(x1,x2),mean=mu1, sigma=sig*1) +
+#   0.6*dmvnorm(cbind(x1,x2),mean=mu2, sigma=sig*2)
+# })
+# plot_ly(x = x1, y = x2, z = 95+2000*fx) %>% add_surface()
+# 
+# set.seed(3)
+# s_ind = sort(sample(n^2,ns))
+# zval = rep(0, ns)
+# k = 1
+# for ( i in s_ind)
+# {
+#   zval[k] =fx[i]
+#   k = k + 1
+# }
+# s_col = s_ind%/%n + 1 ; s_col[s_ind%/%n == n] = n
+# s_row = s_ind%%n + 1 ; s_row[s_row == 0] = n
+# # par_mu = 85+zval*5000 # setting3_1 : 85~113, nBS=5
+# # par_mu = 90+zval*4000 # setting3_2 : 90~112, nBS=5
 # par_mu = 95+zval*2000 # setting3_3 : 95~107, nBS=5
-range(par_mu)
-df_mu = data.frame(x1=x1[s_col],x2=x2[s_row],par_mu=par_mu)
-plot3d(x=df_mu$x1,y=df_mu$x2,z=par_mu)
+# range(par_mu)
+# df_mu = data.frame(x1=x1[s_col],x2=x2[s_row],par_mu=par_mu)
+# plot3d(x=df_mu$x1,y=df_mu$x2,z=par_mu)
 
 ######################################################################
 set.seed(2)
